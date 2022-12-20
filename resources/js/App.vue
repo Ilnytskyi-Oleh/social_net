48040bbc3c26a7d2920a490b9426673dfaedc7dc
Oleg Ilnytskyy <ilnytskyy67922@gmail.com>
2022-12-20 09:54:44 +0200

    base func

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link :to="{name: 'Index'}">Index</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Index Component
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
