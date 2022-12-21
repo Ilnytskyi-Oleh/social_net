135da84bc3e7110fc2bf136bf88e248f53d59b22
Oleg Ilnytskyy <ilnytskyy67922@gmail.com>
2022-12-21 17:14:55 +0200

    +Create Post form

--- resources/js/views/user/Personal.vue
+++ resources/js/views/user/Personal.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="w-96 mx-auto">
-            Personal
+    <div class="w-96 mx-auto p-4">
+        <div class="space-y-5">
+            <div>
+                <input v-model="title" type="text" placeholder="title" class="input input-bordered input-primary w-full max-w-xs" />
+            </div>
+            <div>
+                <textarea v-model="content" name="content" placeholder="content" class="textarea textarea-primary w-full max-w-xs"></textarea>
+            </div>
+            <div>
+                <a href="#" @click.prevent="store" class="btn btn-primary">Publish</a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,8 +18,20 @@
 
 export default {
     name: "Personal",
-
-
+    data(){
+      return {
+          title:'',
+          content:'',
+      }
+    },
+    methods:{
+        store(){
+            axios.post('/api/post', {title: this.title, content: this.content})
+                .then( res => {
+                    console.log(res.data)
+                })
+        }
+    }
 }
 </script>
 
